24f35ded06e889e9d1518e48efe916d0f936cc9c
chanchal410 <cdnawani29@gmail.com>
2026-06-04 16:16:14 +0500

    Add files via upload

---
+++ fibonacci series.py
@@ -0,0 +1,11 @@
+# Fibonacci Generator
+
+n = int(input("Enter the number of terms: "))
+
+a, b = 0, 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
